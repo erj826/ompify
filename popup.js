a93eb53191d4f73c1f8429782901d1b2c65591c4
Eric Jacobson <erj826@bu.edu>
2020-02-21 13:57:01 -0500

    v1

---
+++ popup.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("ompifyMe").addEventListener('click', ompText, false);
+    function ompText() {
+      chrome.tabs.getSelected(null, function(tab) {
+      function ompify() {
+        d = document.querySelectorAll('h1, h2, h3, h4, h5, p, span');
+        for (var i = 0; i < d.length; i++) {
+          var text = d[i].innerText.split(' ');
+          for (var j = 0; j < text.length; j++) {
+            if ((Math.random() >= 0.8) && (text[j].length > 2)) {
+              text[j] = text[j].substring(0, text[j].length - 2).concat('omp');
+            }
+          } 
+          d[i].innerText = text.join(' ');
+        }
+      }
+      chrome.tabs.executeScript({
+          code: '(' + ompify + ')();'
+      });
+      });
+    };
+
+    document.getElementById("huzzMe").addEventListener('click', huzzText, false);
+    function huzzText() {
+      chrome.tabs.getSelected(null, function(tab) {
+      function huzzify() {
+        d = document.querySelectorAll('h1, h2, h3, h4, h5, p, span');
+        for (var i = 0; i < d.length; i++) {
+          var text = d[i].innerText.split(' ');
+          for (var j = 0; j < text.length; j++) {
+            if ((Math.random() >= 0.8) && (text[j].length > 2)) {
+              text[j] = 'huzz'.concat(text[j].substring(2, text[j].length));
+            }
+          } 
+          d[i].innerText = text.join(' ');
+        }
+      }
+      chrome.tabs.executeScript({
+          code: '(' + huzzify + ')();'
+      });
+      });
+    };
+});
