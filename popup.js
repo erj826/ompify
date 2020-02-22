583ae042aa27d7082cd77979df709b8439a4f472
erj826 <erj826@bu.edu>
2020-02-22 10:45:20 -0500

    js refactor

--- popup.js
+++ popup.js
@@ -1,43 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("ompMe").addEventListener('click', ompText, false);
-    function ompText() {
-      chrome.tabs.getSelected(null, function(tab) {
-      function ompify() {
-        d = document.querySelectorAll('h1, h2, h3, h4, h5, p, span');
-        for (var i = 0; i < d.length; i++) {
-          var text = d[i].innerText.split(' ');
-          for (var j = 0; j < text.length; j++) {
-            if ((Math.random() >= 0.7) && (text[j].length > 2)) {
-              text[j] = text[j].substring(0, text[j].length - 2).concat('omp');
-            }
-          } 
-          d[i].innerText = text.join(' ');
-        }
-      }
-      chrome.tabs.executeScript({
-          code: '(' + ompify + ')();'
-      });
-      });
-    };
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById("huzzMe").addEventListener('click', modifyText.bind(null, 'huzz'), false);
+  document.getElementById("ompMe").addEventListener('click', modifyText.bind(null, 'omp'), false);
 
-    document.getElementById("huzzMe").addEventListener('click', huzzText, false);
-    function huzzText() {
-      chrome.tabs.getSelected(null, function(tab) {
-      function huzzify() {
-        d = document.querySelectorAll('h1, h2, h3, h4, h5, p, span');
+  function modifyText(str) {
+    chrome.tabs.getSelected(null, function (tab) {
+      function huzzompify() {
+        const HUZZ = 'huzz';
+        const OMP = 'omp';
+        const modifier = objModifier.str;
+        const d = document.querySelectorAll('h1, h2, h3, h4, h5, p, span');
         for (var i = 0; i < d.length; i++) {
           var text = d[i].innerText.split(' ');
           for (var j = 0; j < text.length; j++) {
-            if ((Math.random() >= 0.7) && (text[j].length > 2)) {
-              text[j] = 'huzz'.concat(text[j].substring(2, text[j].length));
+            var word = text[j];
+            var wordLen = word.length
+            if ((Math.random() >= 0.7) && (wordLen > 2)) {
+              if (modifier === OMP) {
+                text[j] = word.substring(0, wordLen - 2).concat(OMP);
+              } else if (modifier === HUZZ) {
+                text[j] = HUZZ.concat(word.substring(2, wordLen));
+              }
             }
-          } 
+          }
           d[i].innerText = text.join(' ');
         }
       }
-      chrome.tabs.executeScript({
-          code: '(' + huzzify + ')();'
+      chrome.tabs.executeScript({code: 'var objModifier = ' + JSON.stringify({str}) + ';'}, 
+        function() {
+          chrome.tabs.executeScript({code: '(' + huzzompify + ')()'});
       });
-      });
-    };
+    });
+  };
 });
